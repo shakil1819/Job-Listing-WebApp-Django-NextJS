b6b37ddd37b48499fc55561d2f5d541943add3ba
Shakil Mosharrof <58840439+shakil1819@users.noreply.github.com>
2024-05-29 22:36:32 +0000

    Completed project : backend folder

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,8 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenVerifyView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('job.urls')),
+    path('api/', include('account.urls')),
+    path('api/token', TokenObtainPairView.as_view()),
+    path('api/token/verify', TokenVerifyView.as_view())
 ]
+
+
+handler500 = 'utils.error_views.handler500'
+handler404 = 'utils.error_views.handler404'
